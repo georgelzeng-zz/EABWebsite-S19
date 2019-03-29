0017c4eb229b6d214d164fd676e5e86d05b3b275
Kyle Ngo <Quichey@Kyles-MacBook-Pro-2.local>
2019-03-29 05:09:07 -0700

    added basic functionality for searching users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,22 @@ class UsersController < ApplicationController
   end
 
   def index
+    if current_user
+      @message = "Hello, #{current_user.email}!"
+      @users = User.all
+    else
+      redirect_to home_path
+    end
   end
 
   def show
-    redirect_to home_path
+    if current_user
+      @message = "Hello, #{current_user.email}!"
+      id = params[:id] # retrieve movie ID from URI route
+      @user = User.find(id) # look up movie by unique ID
+    else
+      @message = "You aren't logged in!"
+    end
   end
 
   def login
