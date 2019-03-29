773a85e594323e5da068194bc794b83253863f09
Kyle Ngo <Quichey@Kyles-MacBook-Pro-2.local>
2019-03-29 04:27:19 -0700

    log out button working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
 
-
   def home
+    if current_user
+      @message = "Hello, #{current_user.email}!"
+    else
+      @message = "You aren't logged in!"
+    end
   end
 
   def index
   end
 
   def show
+    redirect_to home_path
   end
 
   def login
