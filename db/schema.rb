903bb73b66e471999e3fca9d282f81a664d07ed9
Kyle Ngo <Quichey@Kyles-MacBook-Pro-2.local>
2019-03-28 23:49:45 -0700

    added devise folders to edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190329063039) do
+ActiveRecord::Schema.define(version: 20190329064103) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first"
