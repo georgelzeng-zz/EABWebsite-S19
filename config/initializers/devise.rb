645ba0b4c5d013309944ef4e7f5910e6adb1bdb3
Kyle Ngo <Quichey@Kyles-MacBook-Pro-2.local>
2019-03-30 16:11:51 -0700

    attempting to fix password-change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = ENV["GMAIL_USERNAME"]
+  config.mailer = 'Devise::Mailer'
 
   config.scoped_views = true
   # Configure the class responsible to send e-mails.
