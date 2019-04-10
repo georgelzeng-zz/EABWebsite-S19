5481bda80ceeb8d746ae31d7140ae8644fcba0ce
georgelzeng <georgelzeng@berkeley.edu>
2019-04-10 15:31:02 -0700

    profile pictures can now be added, there is an edit profile page, unfortuntately requires password for user everytime

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ActiveRecord::Base
 	validates :sid, uniqueness: true
 	validates :first, :last, :email, :sid, presence: true
   validate :correct_access_code
-  @@registration_code = ENV["ACCESS_CODE"]
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
+  @@registration_code = "Michael"
   @@admin_code = ENV["ADMIN_CODE"]
 
   def correct_access_code
