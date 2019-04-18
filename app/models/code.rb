2834512fdef1e0a59004312bc0ac0e54b75efe1f
quichey <kylengo357@gmail.com>
2019-04-18 03:35:11 -0700

    organized Code model

--- app/models/code.rb
+++ app/models/code.rb
@@ -3,12 +3,16 @@ class Code < ActiveRecord::Base
   validates :code, uniqueness: true
   validate :correct_type
 
+
+  #Custom validation methods
   def correct_type
     if self.code_type != "regular" && self.code_type != "admin"
       errors.add(:code_type, "-- Not valid access code type")
     end
   end
 
+
+  #Strings for flash messages
   def self.code_uniqueness_message
     "Code-change failed: Access codes must be different from each other."
   end
@@ -21,6 +25,8 @@ class Code < ActiveRecord::Base
     "#{type} Code successfully changed from \"#{old_code}\" to \"#{new_code}\""
   end
 
+
+  #Getter methods
   def self.regular_code
     if Code.where(code_type: "regular")[0] == nil
       Code.create!(code_type: "regular", code: ENV["ACCESS_CODE"])
@@ -37,6 +43,8 @@ class Code < ActiveRecord::Base
     return Code.where(code_type: "admin")[0].code
   end
 
+
+  #Setter_methods
   def self.set_regular_code(newCode)
     code = Code.where(code_type: "regular")[0]
     code.code = newCode
