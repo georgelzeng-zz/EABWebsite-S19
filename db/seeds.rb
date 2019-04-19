8a476005c1ceeacb5f758eae31c10dda62d347e7
quichey <kylengo357@gmail.com>
2019-04-19 00:59:41 -0700

    added more regular users, may not have real emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,10 @@ User.create!(first: 'Jason', last: 'Bi', email: 'bi.jason13@berkeley.edu', team:
 User.create!(first: 'Chau', last: 'Van', email: 'c.van@berkeley.edu', team: 'kiwi', skillset: 'None', sid: '87654921', password: 'pancakes', password_confirmation: 'pancakes', code: Code.admin_code)
 User.create!(first: 'Michael', last: 'Wu', email: 'wuxiaohua1011@berkeley.edu', team: 'kiwi', skillset: 'None', sid: '47654321', password: 'pancakes', password_confirmation: 'pancakes', code: Code.admin_code)
 User.create!(first: 'Nick', last: 'Cai', email: 'ncaia@berkeley.edu', team: 'kiwi', skillset: 'None', sid: '87654301', password: 'pancakes', password_confirmation: 'pancakes', code: Code.admin_code)
+
 User.create!(first: 'Bobby', last: 'James', email: 'kylengo357@gmail.com', team: 'dog', skillset: 'None', sid: '47054391', password: 'pancakes', password_confirmation: 'pancakes', code: Code.regular_code)
+User.create!(first: 'Jonathan', last: 'Hill', email: 'jonhill@gmail.com', team: 'dog', skillset: 'None', sid: '4629957097', password: 'pancakes', password_confirmation: 'pancakes', code: Code.regular_code)
+User.create!(first: 'Jennifer', last: 'White', email: 'jennifer123@gmail.com', team: 'dog', skillset: 'None', sid: '2174600215', password: 'pancakes', password_confirmation: 'pancakes', code: Code.regular_code)
+
+User.create!(first: 'Eric', last: 'Brown', email: 'ebrown@gmail.com', team: 'chair', skillset: 'None', sid: '8241025001', password: 'pancakes', password_confirmation: 'pancakes', code: Code.regular_code)
 User.create!(first: 'Anna', last: 'Smith', email: 'blankityblankbizmark@gmail.com', team: 'chair', skillset: 'None', sid: '38654797', password: 'pancakes', password_confirmation: 'pancakes', code: Code.regular_code)
