95cc73b2e4145cb45d81c1b4e184ebff14fdffb2
Jason Bi <bi.jason13@gmail.com>
2019-04-25 15:56:58 -0700

    Added upload size limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :image, :less_than => 5.megabytes
+
 
   @@roster_file_name = "EAB_roster.xml"
   @@roster_file_directory = Rails.root.to_s
