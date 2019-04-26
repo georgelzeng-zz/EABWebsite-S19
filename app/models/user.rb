851a6a8874572b9901cf24feaf78d2355f35bf2b
Jason Bi <bi.jason13@gmail.com>
2019-04-25 20:06:05 -0700

    Autocomplete search in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  validates_attachment_size :image, :less_than => 5.megabytes
+    validates_attachment_size :image, :less_than => 5.megabytes
 
 
   @@roster_file_name = "EAB_roster.xml"
@@ -57,6 +57,7 @@ class User < ActiveRecord::Base
       else
         @@results = self.search_phrase(search, admin)
       end
+
     else
       all.order(:first)
     end
