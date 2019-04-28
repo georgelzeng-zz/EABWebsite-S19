4e81dd188fb1f58d6fee1b17bab429f5bfb7d38a
Nicholas Cai <ncai@berkeley.edu>
2019-04-28 09:23:47 -0700

    updated tiles, styled login

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20190427193456) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "admin"
