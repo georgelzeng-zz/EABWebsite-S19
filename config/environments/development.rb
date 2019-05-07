b1f9962ef1b08f748ecf04eb80f32c1fb8005438
quichey <kylengo357@gmail.com>
2019-05-07 05:41:59 -0700

    recover-password email working in development, need to test for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp-relay.gmail.com",
+    address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
