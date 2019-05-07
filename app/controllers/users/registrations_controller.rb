ae7ace0b39331f5e7f469d46f5c300dbff4fde46
Jason Bi <bi.jason13@gmail.com>
2019-05-07 01:03:53 -0700

    Password confirmation optional started

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -33,6 +33,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     redirect_to users_path, flash: {notice: "User was successfully deleted"}
   end
 
+  
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
