00fa88f2a2f903b73d60645e0eb5f698912e5eaa
quichey <kylengo357@gmail.com>
2019-05-08 04:43:07 -0700

    changed configuration to be amenable to berkeley.edu emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,13 +44,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: ENV["SERVER_ADDRESS"],
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["EMAIL_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"]
   }
 
   # Raises error for missing translations
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
   #config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: ENV["SERVER_ADDRESS"],
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["EMAIL_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"]
   }
 end
