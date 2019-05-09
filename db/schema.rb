951e11e34889f888f8d56678c74b2952ae5ac0b4
Chau Van <c.van@berkeley.edu>
2019-05-09 16:51:12 -0700

    announcements model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190427213816) do
+ActiveRecord::Schema.define(version: 20190509234401) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "codes", force: :cascade do |t|
     t.string   "code_type"
