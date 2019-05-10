24a9568146a5d06b64b5f97ca972e4604547482b
Chau Van <c.van@berkeley.edu>
2019-05-09 17:51:35 -0700

    temp save update

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,4 +1,18 @@
 class AnnouncementsController < ApplicationController
   def index
   end
+
+  def new
+    @announcement = Announcement.new
+  end
+
+  def create
+    @announcement = Announcemeent.new(content_params)
+  end
+
+  private
+    def content_params
+      params.require(:announcement).permit(:title, :description)
+    end
+
 end
